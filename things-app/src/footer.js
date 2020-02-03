2bdab1105a87fdb5312ec651d060901c18659916
glasscharlie <charlie-glass@live.com>
2020-02-03 08:24:39 -0800

    started project

---
+++ things-app/src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export default props => (
+    <>
+        <p>Footer stuffs {props.copyright} </p>
+    </>
+)
+
+// function Footer(props) {
+//     return  (
+//     <>
+//         <p>Footer stuffs</p>
+//     </>
+//     )
+// }
+// export dafault Footer
